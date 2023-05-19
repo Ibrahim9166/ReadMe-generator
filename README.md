cb51f3d2a6818a14c3aa9d5fc7d788c117c6f36c
ibrahim <ibrahimmuhieddine02@gmail.com>
2023-05-18 20:05:32 -0700

    i tried to finish the index.js file with adding all the questions and creating the functions this could be completely wrong

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
-# ReadMe-generator
+# ReadMe Generator
+  
+  ## Description
+  generates readme files to save time on any project
+  
+  ## Table of Contents
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [License](#license)
+  - [Contributing](#contributing)
+  - [Tests](#tests)
+  - [Questions](#questions)
+  
+  ## Installation
+  download app and run it through node.js
+  
+  ## Usage
+  usage is for a quicker and reliable way to make readme files
+  
+  ## License
+  This project is covered under the None license.
+  
+  ## Contributing
+  undefined
+  
+  ## Tests
+  
+  
+  ## Questions
+  If you have any questions, you can reach out to me via GitHub or email:
+  - GitHub: [ibrahim9166](https://github.com/ibrahim9166)
+  - Email: ibrahimmuhieddine02@gmail.com
+  
--- index.js
+++ index.js
@@ -1,13 +1,138 @@
 // TODO: Include packages needed for this application
+const fs = require('fs');
+const inquirer = require('inquirer');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'Enter the project title:',
+      },
+      {
+        type: 'input',
+        name: 'description',
+        message: 'Enter the project description:',
+      },
+      {
+        type: 'input',
+        name: 'installation',
+        message: 'Enter the installation instructions:',
+      },
+      {
+        type: 'input',
+        name: 'usage',
+        message: 'Enter the usage information:',
+      },
+      {
+        type: 'input',
+        name: 'contribution',
+        message: 'Enter the contribution guidelines:',
+      },
+      {
+        type: 'input',
+        name: 'tests',
+        message: 'Enter the test instructions:',
+      },
+      {
+        type: 'list',
+        name: 'license',
+        message: 'Choose a license for your project:',
+        choices: ['MIT', 'Apache 2.0', 'GPL 3.0', 'BSD 3-Clause', 'None'],
+      },
+      {
+        type: 'input',
+        name: 'githubUsername',
+        message: 'Enter your GitHub username:',
+      },
+      {
+        type: 'input',
+        name: 'email',
+        message: 'Enter your email address:',
+      },
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+  fs.writeFile(fileName, data, (err) => {
+    if (err) {
+      console.error(err);
+    } else {
+      console.log('README.md file generated successfully!');
+    }
+  });
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+  inquirer
+    .prompt(questions)
+    .then((answers) => {
+      // Generate the README content using the user's answers
+      const readmeContent = generateReadmeContent(answers);
 
-// Function call to initialize app
-init();
+      // Write the README file
+      writeToFile('README.md', readmeContent);
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+}
+
+// TODO: Create a function to generate README content based on user input
+function generateReadmeContent(answers) {
+    // Use the user's answers to construct the README content
+    const {
+      title,
+      description,
+      installation,
+      usage,
+      license,
+      contributing,
+      tests,
+      githubUsername,
+      email,
+    } = answers;
+  
+    // Construct the README content using Markdown syntax
+    let readmeContent = `# ${title}
+  
+  ## Description
+  ${description}
+  
+  ## Table of Contents
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [License](#license)
+  - [Contributing](#contributing)
+  - [Tests](#tests)
+  - [Questions](#questions)
+  
+  ## Installation
+  ${installation}
+  
+  ## Usage
+  ${usage}
+  
+  ## License
+  This project is covered under the ${license} license.
+  
+  ## Contributing
+  ${contributing}
+  
+  ## Tests
+  ${tests}
+  
+  ## Questions
+  If you have any questions, you can reach out to me via GitHub or email:
+  - GitHub: [${githubUsername}](https://github.com/${githubUsername})
+  - Email: ${email}
+  `;
+  
+    // Return the generated README content as a string
+    return readmeContent;
+  }
+  
+  // Function call to initialize app
+  init();
+  
